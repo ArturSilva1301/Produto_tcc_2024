98e836f5d705fa137627708bac0c0059d480df55
ARTUR DA SILVA <04266993005@senacrs.edu.br>
2024-11-01 08:34:13 -0300

    tela home feita e funcionando

---
+++ Front_End/home/home.js
@@ -0,0 +1,9 @@
+// Redireciona para a página de cadastro da escola
+document.getElementById('botaoEscola').addEventListener('click', function() {
+    window.location.href = "../Cadastro_escola/cadastro.html";
+});
+
+// Redireciona para a página de cadastro do responsável
+document.getElementById('botaoResponsavel').addEventListener('click', function() {
+    window.location.href = "../Cadastro_responsavel/cadastro_inicio.html";
+});
